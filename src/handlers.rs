3d80f9ea33c15815561ccdef29c4bc3696e9370a
Jace Babin <jbabin91@gmail.com>
2023-10-26 23:21:06 -0700

    Add in read handler for quotes

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,61 +1,74 @@
 use axum::{extract, http};
 use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
+use sqlx::{FromRow, PgPool};
 
-#[derive(Serialize)]
+#[derive(Serialize, FromRow)]
 pub struct Quote {
-  id: uuid::Uuid,
-  book: String,
-  quote: String,
-  inserted_at: chrono::DateTime<chrono::Utc>,
-  updated_at: chrono::DateTime<chrono::Utc>,
+    id: uuid::Uuid,
+    book: String,
+    quote: String,
+    inserted_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
 }
 
 impl Quote {
-  fn new(book: String, quote: String) -> Self {
-    let now = chrono::Utc::now();
-    Self {
-      id: uuid::Uuid::new_v4(),
-      book,
-      quote,
-      inserted_at: now,
-      updated_at: now,
+    fn new(book: String, quote: String) -> Self {
+        let now = chrono::Utc::now();
+        Self {
+            id: uuid::Uuid::new_v4(),
+            book,
+            quote,
+            inserted_at: now,
+            updated_at: now,
+        }
     }
-  }
 }
 
 #[derive(Debug, Deserialize)]
 pub struct CreateQuote {
-  book: String,
-  quote: String,
+    book: String,
+    quote: String,
 }
 
 pub async fn health() -> http::StatusCode {
-  http::StatusCode::OK
+    http::StatusCode::OK
 }
 
 pub async fn create_quote(
-  extract::State(pool): extract::State<PgPool>,
-  axum::Json(payload): axum::Json<CreateQuote>,
+    extract::State(pool): extract::State<PgPool>,
+    axum::Json(payload): axum::Json<CreateQuote>,
 ) -> Result<(http::StatusCode, axum::Json<Quote>), http::StatusCode> {
-  let quote = Quote::new(payload.book, payload.quote);
+    let quote = Quote::new(payload.book, payload.quote);
 
-  let res = sqlx::query(
-    r#"
+    let res = sqlx::query(
+        r#"
     INSERT INTO quotes (id, book, quote, inserted_at, updated_at)
     VALUES ($1, $2, $3, $4, $5);
     "#,
-  )
-  .bind(&quote.id)
-  .bind(&quote.book)
-  .bind(&quote.quote)
-  .bind(&quote.inserted_at)
-  .bind(&quote.updated_at)
-  .execute(&pool)
-  .await;
+    )
+    .bind(&quote.id)
+    .bind(&quote.book)
+    .bind(&quote.quote)
+    .bind(&quote.inserted_at)
+    .bind(&quote.updated_at)
+    .execute(&pool)
+    .await;
 
-  match res {
-    Ok(_) => Ok((http::StatusCode::CREATED, axum::Json(quote))),
-    Err(_) => Err(http::StatusCode::INTERNAL_SERVER_ERROR),
-  }
+    match res {
+        Ok(_) => Ok((http::StatusCode::CREATED, axum::Json(quote))),
+        Err(_) => Err(http::StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+pub async fn read_quotes(
+    extract::State(pool): extract::State<PgPool>,
+) -> Result<axum::Json<Vec<Quote>>, http::StatusCode> {
+    let res = sqlx::query_as::<_, Quote>("SELECT * FROM quotes")
+        .fetch_all(&pool)
+        .await;
+
+    match res {
+        Ok(quotes) => Ok(axum::Json(quotes)),
+        Err(_) => Err(http::StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,26 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
-  let addr = format!("0.0.0.0:{}", port);
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr = format!("0.0.0.0:{}", port);
 
-  let database_url = env::var("DATABASE_URL").expect("Missing DATABASE_URL env");
+    let database_url = env::var("DATABASE_URL").expect("Missing DATABASE_URL env");
 
-  let pool = PgPoolOptions::new()
-    .max_connections(5)
-    .connect(&database_url)
-    .await?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
 
-  let app = Router::new()
-    .route("/", get(handlers::health))
-    .route("/quotes", post(handlers::create_quote))
-    .with_state(pool);
+    let app = Router::new()
+        .route("/", get(handlers::health))
+        .route("/quotes", post(handlers::create_quote))
+        .route("/quotes", get(handlers::read_quotes))
+        .with_state(pool);
 
-  axum::Server::bind(&addr.parse().unwrap())
-    .serve(app.into_make_service())
-    .await
-    .unwrap();
+    axum::Server::bind(&addr.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 
-  Ok(())
+    Ok(())
 }
-
